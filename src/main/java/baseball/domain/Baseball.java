7bed051b7eb655ad9414a96f00c5d4081fa5d401
bomingeem <bomingeem@gmail.com>
2022-09-27 22:52:09 +0900

    feat(Baseball.java): Baseball 도메인 추가

---
+++ src/main/java/baseball/domain/Baseball.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class Baseball {
+    private int ballCount;
+    private int strikeCount;
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+}
