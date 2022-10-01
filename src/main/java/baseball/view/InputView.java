8e0a26020659ebc1c131df73db4ee876a20e5796
bomingeem <bomingeem@gmail.com>
2022-10-01 20:00:59 +0900

    feat: 출력 및 입력을 위한 View 클래스 생성

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class InputView {
+    public static void printInputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printRestartGame() {
+        System.out.print("게임을 새로 새직하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
